2bc4e95b268f0b7d66e0ab23a08cedc72e75e41c
Aditya Kamat <GiVeMeRoOt@users.noreply.github.com>
2016-07-16 22:00:00 +0530

    Update mySolution.py

--- abctf/Fridays/mySolution.py
+++ abctf/Fridays/mySolution.py
@@ -3,34 +3,32 @@
 
 import urllib2 
 
+
 data = urllib2.urlopen('https://gist.githubusercontent.com/LiamRahav/0cbf82b8bc3a41deafc0403f1b1dd573/raw/cfdd458378bf2da29575f3b275ca343c1ec501bc/date.txt')
+
 month1 = {"January" : '01', "February" : '02', "March" : '03', "April" : '04', "May" : '05', "June" : '06', "July" : '07', "August" : '08', "September" : '09', "October" : '10', "November" : '11', "December" : '12'}  
 
-def weekDay(year, month, day):
-    offset = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
-    week   = ['Sunday', 
-              'Monday', 
-              'Tuesday', 
-              'Wednesday', 
-              'Thursday',  
-              'Friday', 
-              'Saturday']
-    afterFeb = 1
-    if month > 2: afterFeb = 0
-    aux = year - 1700 - afterFeb
-    # dayOfWeek for 1700/1/1 = 5, Friday
-    dayOfWeek  = 5
-    # partial sum of days betweem current date and 1700/1/1
-    dayOfWeek += (aux + afterFeb) * 365                  
-    # leap year correction    
-    dayOfWeek += aux / 4 - aux / 100 + (aux + 100) / 400     
-    # sum monthly and day offsets
-    dayOfWeek += offset[month - 1] + (day - 1)               
-    dayOfWeek %= 7
-    return week[dayOfWeek]
 
-
+days =[ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
+       'Sunday' ]
 count = 0
+
+def datetoday(day, month, year):
+    d = day
+    m = month
+    y = year
+    if m < 3:
+        z = y-1
+    else:
+        z = y
+    dayofweek = ( 23*m//9 + d + 4 + y + z//4 - z//100 + z//400 )
+    if m >= 3:
+        dayofweek -= 2
+    dayofweek = dayofweek%7
+    return dayofweek
+
+
+
 for i in data:
 	i = i.replace(",","")
 	date1 = i.split()
@@ -38,11 +36,10 @@ for i in data:
 		if date1[0] in j:
 			date1[0] = month1.get(j,"default") 
 	date1[2] = int(date1[2]) + 1
-	#my_date = int(date1[2])+"-"+int(date1[0])+"-"+int(date1[1])
-	#calendar.day_name[my_date.weekday()]
-	#print date1
-	res = weekDay(int(date1[2]), int(date1[0]), int(date1[1]))	
-	if res == "Friday":
+	dayofweek = days[datetoday(int(date1[1]), int(date1[0]), int(date1[2]))-1]
+
+	print dayofweek	
+	if dayofweek == "Friday":
 		count += 1
 	
 
